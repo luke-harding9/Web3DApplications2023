839bbd378bc77bd1856a42ac4dbfbf5a1ce70ca6
luke-harding9 <133869917+luke-harding9@users.noreply.github.com>
2023-05-17 12:46:43 +0100

    Add files via upload

---
+++ 3dapp/assignment/js/swap_restyle.js
@@ -0,0 +1,50 @@
+var counter = 0;
+
+function swap(selected){
+
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('coke').style.display = 'none';
+    document.getElementById('sprite').style.display = 'none';
+    document.getElementById('pepper').style.display = 'none';
+    document.getElementById(selected).style.display = 'block';
+}
+
+function changeLook()
+{
+    counter += 1;
+
+    switch (counter)
+    {
+        case 1:
+            document.getElementById('bodyColor').style.backgroundColor = 'lightblue';
+            document.getElementById('headerColor').style.backgroundColor = '#FF0000';
+            document.getElementById('footerColor').style.backgroundColor = '#FF9900';
+        break;
+
+        case 2:
+            document.getElementById('bodyColor').style.backgroundColor = '#FF6600';
+            document.getElementById('headerColor').style.backgroundColor = '#FF9999';
+            document.getElementById('footerColor').style.backgroundColor = '#996699';
+        break;
+
+        case 3:
+            document.getElementById('bodyColor').style.backgroundColor = 'coral';
+            document.getElementById('headerColor').style.backgroundColor = 'darkcyan';
+            document.getElementById('footerColor').style.backgroundColor = 'darksalmon';
+        break;
+
+        case 4:
+            counter = 0;
+            document.getElementById('bodyColor').style.backgroundColor = 'lightgrey';
+            document.getElementById('headerColor').style.backgroundColor = 'chocolate';
+            document.getElementById('footerColor').style.backgroundColor = 'dimgrey';
+        break;
+    }
+}
+
+function changeBack()
+{
+    document.getElementById('bodyColor').style.backgroundColor = '#FFFFFF';
+    document.getElementById('headerColor').style.backgroundColor = 'rgba(175,0,0,1)';
+    document.getElementById('footerColor').style.backgroundColor = 'rgba(175,0,0,1)';
+}
